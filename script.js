a029fd016f76c922863fa05dc52a8141f9fa645e
lastdeve <65189211+lastdeve@users.noreply.github.com>
2023-02-07 23:05:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,27 +10,30 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-function getWeather() {
-  navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const API_KEY = "866b77a685a00860c5173d53b321ff7d";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        if (data.cod === 200) {
-          const temperature = Math.round(data.main.temp - 273.15);
-          const weatherDescription = data.weather[0].description;
-  
-          document.getElementById("weather").innerHTML = `${temperature}°C, ${weatherDescription}`;
-        } else {
-          document.getElementById("weather").innerHTML = "Kunde inte hämta väderinformation";
-        }
-      })
-      .catch(error => console.error(error));
+async function getWeather() {
+  const position = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
+
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const API_key = "866b77a685a00860c5173d53b321ff7d";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.cod === 200) {
+      const temperature = Math.round(data.main.temp - 273.15);
+      const weatherDescription = data.weather[0].description;
+  
+      document.getElementById("weather").innerHTML = `${temperature}°C, ${weatherDescription}`;
+    } else {
+      document.getElementById("weather").innerHTML = "Kunde inte hämta väderinformation";
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getWeather();

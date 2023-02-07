58acdec998f60d51a70d212bb7633d901d2013c3
lastdeve <65189211+lastdeve@users.noreply.github.com>
2023-02-07 22:38:29 +0100

    update

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const weekdays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
 const today = new Date();
 const dayOfWeek = weekdays[today.getDay()];
-const currentTime = today.toLocaleTimeString();
 
-document.getElementById("day-of-week-time").innerHTML = `${dayOfWeek} ${currentTime}`;
+function updateTime() {
+  const currentTime = new Date().toLocaleTimeString();
+  document.getElementById("day-of-week-time").innerHTML = `${dayOfWeek} ${currentTime}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
 
 // Example using OpenWeatherMap API to get weather information
-const API_KEY = "80ca243099f9b82058054fce2dd0a565";
-const city = "Stockholm";
+const API_KEY = "Y80ca243099f9b82058054fce2dd0a565";
+const city = "stockholm";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
 fetch(url)
